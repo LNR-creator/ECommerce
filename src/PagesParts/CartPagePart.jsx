5b97673d84b9df5ce3d59598548fd3eee62b5220
Pasula Laxmi Narasimha Raju <lnraju366@gmail.com>
2026-02-17 21:28:41 +0530

    second-commit

--- src/PagesParts/CartPagePart.jsx
+++ src/PagesParts/CartPagePart.jsx
@@ -1,29 +1,55 @@
 import React from 'react'
 import '../styles/CartLayout.css'
+import img from '../assets/images/headphones.webp'
 
 const CartPagePart = () => {
   return (
     <div>
       <div className="cart-layout">
         <div className="orders">
+          <h1>MY CART</h1>
+          <br />
           <div className="order-1">
             <div className="img">
-              <img alt="" />
+              <img className='abcd' src={img}/>
+            </div>
+            
+            <div className="product-details">
+              <div className="product-name">Wired Headphones</div>
             </div>
           </div>
 
+            <div className="order-1">
+            <div className="img">
+              <img className='abcd' src={img}/>
+            </div>
+            
+            <div className="product-details">
+              <div className="product-name">Wired Headphones</div>
+            </div>
+          </div>
 
+           <div className="order-1">
+            <div className="img">
+              <img className='abcd' src={img}/>
+            </div>
+            
+            <div className="product-details">
+              <div className="product-name">Wired Headphones</div>
+            </div>
+          </div>
         </div>
         <div className="summary">
             <div className="order-summary">
-                <h1>Total</h1>
-
+               
                 <div className="total">
+                   <h1>Total</h1>
+
                 <p>₹1,000</p>
                 </div>
 
                 <div className="order-btn">
-                    <button type="button">Order</button>
+                    <button type="button" id='order-btn'>Order</button>
                 </div>
             </div>
         </div>
